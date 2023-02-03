a860851eb0ef452e888b076222e4937ec3642c6b
João Gabriel Mauricio <jgm.joaogma@icloud.com>
2023-02-03 13:47:38 -0300

    Creating FormInput and FormSelect component

---
+++ src/components/Forms/formInput/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import withFormField, { FormFieldProps } from "../formField";
+
+const FormInput: React.FC<React.InputHTMLAttributes<HTMLInputElement> & FormFieldProps> = ({
+   errorMessage,
+   hasError = false,
+   isMandatory = false,
+   labelName,
+   name,
+   ...rest
+}) => {
+   return <input {...rest} className={` b_darkopaque rounded form_input h3`} id={name} />;
+};
+
+export default withFormField(FormInput);
---
+++ src/components/Forms/formSelect/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import FormDropdown, { FormDropdownProps } from "components/inputs/dropdownInput";
+import withFormField from "../formField";
+import { Props } from "react-select";
+
+interface FormSelectProps {
+   hasError?: boolean;
+   labelName: string;
+   errorMessage?: string;
+   isMandatory?: boolean;
+}
+
+const FormSelect: React.FC<Props & FormSelectProps & FormDropdownProps> = ({ hasError, labelName, errorMessage, isMandatory, name, ...rest }) => {
+   return <FormDropdown className={`${hasError ? "b_danger" : "b_gray"}`} id={name} {...rest} />;
+};
+
+export default withFormField(FormSelect);
